e57dce511c1031fb11a86a4bfc29bce39ff0ba32
erfan-karimi <h410mi3@gmail.com>
2024-07-01 15:46:11 +0330

    event crud routes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from routes.users import user_router
+from routes.events import event_router
 import uvicorn
 
 
 app = FastAPI()
 # Register routes
 app.include_router(user_router, prefix="/user", tags=["user"])
+app.include_router(event_router, prefix="/event")
 
 
 if __name__ == "__main__":
