5ff55608b1b7314bf67732c1b707f9cc56222c6a
erfan-karimi <h410mi3@gmail.com>
2024-07-03 13:25:12 +0330

    connect to sqlite db

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from database.connection import conn
+
 from routes.users import user_router
 from routes.events import event_router
 import uvicorn
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the ML model
+    conn()
+    yield
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
+
 # Register routes
 app.include_router(user_router, prefix="/user", tags=["user"])
 app.include_router(event_router, prefix="/event")
